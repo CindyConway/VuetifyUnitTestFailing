e89e5aef228319ee8f02f18bbc7f310fff600420
psychosis448 <sebbbaier@googlemail.com>
2018-06-14 22:44:44 +0200

    fixed vuetify jest

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,58 +1,9 @@
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
-
-import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions,
-  VCard,
-  VExpansionPanel,
-  VMenu,
-  VDialog,
-} from 'vuetify';
-
-const localVue = createLocalVue()
-Vue.use(Vuetify, {
-  components: {
-    VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
-    VBtn,
-    VIcon,
-    VGrid,
-    VToolbar,
-    transitions,
-    VCard,
-    VExpansionPanel,
-    VMenu,
-    VDialog,
-  },
-  theme: {
-    primary: '#424242',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107',
-  },
-});
+import HelloWorld from '@/components/HelloWorld'
+import { mount } from '@vue/test-utils'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-
-    let wrapper = mount(HelloWorld, {
-      localVue,
-    })
-
+    let wrapper = mount(HelloWorld)
     expect(wrapper.find('img').exists()).toBe(true);
-  });
-});
+  })
+})
